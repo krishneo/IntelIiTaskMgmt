96746738edc0444d1c67b9f0b680844fa5861ebc
KrishnaKumar <KrishnaKumar@Neo-Lappie-W10>
2015-11-01 03:12:00 +0530

    Emergency Scenario Accounted For in code

--- src/main/java/com/hackthon/teamwg/projects/dao/TaskAssignmentDAO.java
+++ src/main/java/com/hackthon/teamwg/projects/dao/TaskAssignmentDAO.java
@@ -24,20 +24,40 @@ public class TaskAssignmentDAO {
 	private QueryRunner queryRunner;
 
 	public void runTaskAssignmentAlgorithm(String group_name) throws Exception {
-
+		String emergency_ind = "N";
 		if("ALL".equalsIgnoreCase(group_name))
 		{
 			List<RaasGroupsDTO> groups=getAllGroupsWithUnAssignedTasks();
 			for(RaasGroupsDTO obj:groups)
 			{
 				logger.info("Running Assignement for group "+obj.getGroupName());
-				this.invokeTaskAssignment(obj.getGroupName());
+				this.invokeTaskAssignment(obj.getGroupName(),emergency_ind);
 			}
 		}else
-			this.invokeTaskAssignment(group_name);
+			this.invokeTaskAssignment(group_name, emergency_ind);
 		
 	}
 
+	private void allocateHighPriorityTask(String group_name,String emergency_ind) throws Exception {
+		// GetList Of Users Above user_bound to be assigned
+		List<RaasTasksDTO> unassigned_tasks = this.getListOfHighPriorityTasks(group_name, emergency_ind);
+		// List<RaasUsersDTO> unassigned_users =
+		// this.getListOfUnAssignedUsersInGroup(group_name);
+		RaasUsersDTO curr_user = null;
+		int availablility = 0;
+		for (RaasTasksDTO curr_task : unassigned_tasks) {
+
+			curr_user = getNextUnAssignedUsersInGroupForHighPriorityTask(group_name, emergency_ind);
+			if (curr_user == null)
+				break;
+			logger.info("Assigning user :" + curr_user.getLoginId() + " task " + curr_task.getTask_key());
+			curr_task.setUser_name(curr_user.getLoginId());
+			curr_task.setStatus("ASSIGNED");
+			logger.info("Assigend Task Obj " + curr_task);
+			this.updateTasks(curr_task);
+
+		}		
+	}
 	private List<RaasGroupsDTO> getAllGroupsWithUnAssignedTasks() throws Exception {
 
 		String sql = "SELECT DISTINCT group_name groupName" + "  FROM raas_tasks tasks, raas_groups grp"
@@ -48,19 +68,20 @@ public class TaskAssignmentDAO {
 		return groups;
 	}
 
-	public List<RaasTasksDTO> invokeTaskAssignment(String group_name) throws Exception {
+	public List<RaasTasksDTO> invokeTaskAssignment(String group_name,String emergency_ind) throws Exception {
 
-		List<RaasTasksDTO> unassigned_tasks = this.shuffleTasksAccordingToPriorities(group_name);
-		List<RaasUsersDTO> unassigned_users = this.getListOfUnAssignedUsersInGroup(group_name);
+		this.allocateHighPriorityTask(group_name, emergency_ind);
+		List<RaasTasksDTO> unassigned_tasks = this.shuffleTasksAccordingToPriorities(group_name, emergency_ind);
+		List<RaasUsersDTO> unassigned_users = this.getListOfUnAssignedUsersInGroup(group_name, emergency_ind);
 		RaasTasksDTO curr_task = null;
 		int availablility = 0;
 		for (RaasUsersDTO obj : unassigned_users) {
 			availablility = obj.getAvailablity() != null ? Integer.parseInt(obj.getAvailablity()) : 0;
-			curr_task = getNextUnAssignedTasks(group_name);
+			curr_task = getNextUnAssignedTasks(group_name, emergency_ind);
 			if (curr_task == null)
 				break;
 			for (int i = 1; i <= availablility; i++) {
-				curr_task = getNextUnAssignedTasks(group_name);
+				curr_task = getNextUnAssignedTasks(group_name, emergency_ind);
 
 				if (curr_task != null) {
 					logger.info("Assigning user :" + obj.getLoginId() + " task " + curr_task.getTask_key());
@@ -77,7 +98,7 @@ public class TaskAssignmentDAO {
 		return null;
 	}
 
-	private List<RaasUsersDTO> getListOfUnAssignedUsersInGroup(String group_name) throws Exception {
+	private List<RaasUsersDTO> getListOfUnAssignedUsersInGroup(String group_name,String emergency_ind) throws Exception {
 
 		String sql = "SELECT *" + "    FROM (SELECT in_consolidated_tbl.*,"
 				+ "                 (userBound - assigned_tasks - working_tasks) availablity"
@@ -95,7 +116,7 @@ public class TaskAssignmentDAO {
 				+ "                            finished_tasks"
 				+ "                    FROM raas_users up, raas_group_user_map gum"
 				+ "                   WHERE     user_role != 'MANAGER'"
-				+ "                         AND gum.group_name = '" + group_name + "' "
+				+ "                         AND gum.group_name IN (select name name from raas_groups where name='"+group_name+"' union select child_group_name name from raas_group_map where parent_group_name='"+group_name+"' AND 'Y'='"+emergency_ind+"')"
 				+ "                         AND up.login_id = gum.user_name) in_consolidated_tbl)"
 				+ "         ou_consolidated_tbl" + "   WHERE availablity != 0"
 				+ " ORDER BY working_tasks ASC, availablity DESC, assigned_tasks DESC";
@@ -106,7 +127,48 @@ public class TaskAssignmentDAO {
 		return users;
 	}
 
-	private RaasTasksDTO getNextUnAssignedTasks(String group_name) throws Exception {
+	private RaasUsersDTO getNextUnAssignedUsersInGroupForHighPriorityTask(String group_name,String emergency_ind) throws Exception {
+
+		String sql = "SELECT *" + "    FROM (SELECT in_consolidated_tbl.*,"
+				+ "                 (upperBound - assigned_tasks - working_tasks) availablity"
+				+ "            FROM (SELECT login_id loginId," + "                         user_role userRole,"
+				+ "                         lower_bound lowerBound," + "                         user_bound userBound,"
+				+ "                         max_bound upperBound," + "                         (SELECT count(*)"
+				+ "                            FROM raas_tasks"
+				+ "                           WHERE user_name = login_id AND status = 'ASSIGNED')"
+				+ "                            assigned_tasks," + "                         (SELECT count(*)"
+				+ "                            FROM raas_tasks"
+				+ "                           WHERE user_name = login_id AND status = 'WORKING')"
+				+ "                            working_tasks," + "                         (SELECT count(*)"
+				+ "                            FROM raas_tasks"
+				+ "                           WHERE user_name = login_id AND status = 'COMPLETED')"
+				+ "                            finished_tasks"
+				+ "                    FROM raas_users up, raas_group_user_map gum"
+				+ "                   WHERE     user_role != 'MANAGER'"
+				+ "                         AND gum.group_name IN (select name name from raas_groups where name='"+group_name+"' union select child_group_name name from raas_group_map where parent_group_name='"+group_name+"' AND 'Y'='"+emergency_ind+"')"
+				+ "                         AND NOT EXISTS"
+				+ "                (SELECT 1"
+                + "                   FROM raas_tasks tsk"
+                + "                  WHERE     (   tsk.status = 'ASSIGNED'"
+                + "                             OR tsk.status = 'WORKING')"
+                + "                        AND tsk.user_name = up.login_id"
+                + "                        AND tsk.priority = 10)"
+				+ "                         AND up.login_id = gum.user_name) in_consolidated_tbl)"
+				+ "         ou_consolidated_tbl" + "   WHERE 1 != 2"
+				+ " ORDER BY working_tasks ASC, availablity DESC, assigned_tasks DESC";
+
+		ResultSetHandler<List<RaasUsersDTO>> usageResultSet = new BeanListHandler<RaasUsersDTO>(RaasUsersDTO.class);
+		List<RaasUsersDTO> users = getQueryRunner().query(sql, usageResultSet);
+		if (users.size() > 0) {
+			logger.info(users.get(0));
+			return users.get(0);
+		} else
+			return null;
+		
+	}
+
+	
+	private RaasTasksDTO getNextUnAssignedTasks(String group_name,String emergency_ind) throws Exception {
 
 		String sql = "SELECT" + "        task_oid,task_key," + "            priority," + "            external_sla,"
 				+ "            time_lapse_to_sla" + "    FROM" + "        (SELECT" + "        raas_tasks.task_oid,"
@@ -130,7 +192,7 @@ public class TaskAssignmentDAO {
 			return null;
 	}
 
-	private List<RaasTasksDTO> getListOfUnAssignedTasks(String group_name) throws Exception {
+	private List<RaasTasksDTO> getListOfUnAssignedTasks(String group_name,String emergency_ind) throws Exception {
 
 		String sql = "SELECT" + "        task_oid," + "            priority," + "            external_sla,"
 				+ "            time_lapse_to_sla" + "    FROM" + "        (SELECT" + "        raas_tasks.task_oid,"
@@ -152,9 +214,31 @@ public class TaskAssignmentDAO {
 		return tasks;
 	}
 
-	private List<RaasTasksDTO> shuffleTasksAccordingToPriorities(String group_name) throws Exception {
+	private List<RaasTasksDTO> getListOfHighPriorityTasks(String group_name,String emergency_ind) throws Exception {
 
-		List<RaasTasksDTO> tasks = this.getListOfUnAssignedTasks(group_name);
+		String sql = "SELECT" + "        task_oid," + "            priority," + "            external_sla,"
+				+ "            time_lapse_to_sla" + "    FROM" + "        (SELECT" + "        raas_tasks.task_oid,"
+				+ "            raas_tasks.task_key," + "            raas_tasks.user_name,"
+				+ "            raas_tasks.status," + "            raas_tasks.group_name,"
+				+ "            raas_tasks.created_timestamp," + "            raas_tasks.work_start_timestamp,"
+				+ "            raas_tasks.priority," + "            raas_tasks.internal_sla,"
+				+ "            raas_tasks.external_sla,"
+				+ "            ((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(created_timestamp)) / 3600) time_lapse_to_sla"
+				+ "    FROM" + "        raas_tasks" + "    WHERE" + "        user_name IS NULL AND priority=10 "
+				+ "            AND group_name = '" + group_name + "' "
+				+ "    ORDER BY priority DESC , (NOW() - created_timestamp) DESC) inner_derived_tbl"
+				+ "    ORDER BY priority DESC , (external_sla - time_lapse_to_sla) DESC";
+		ResultSetHandler<List<RaasTasksDTO>> usageResultSet = new BeanListHandler<RaasTasksDTO>(RaasTasksDTO.class);
+
+		List<RaasTasksDTO> tasks = getQueryRunner().query(sql, usageResultSet);
+
+		logger.info(tasks);
+		return tasks;
+	}	
+	
+	private List<RaasTasksDTO> shuffleTasksAccordingToPriorities(String group_name,String emergency_ind) throws Exception {
+
+		List<RaasTasksDTO> tasks = this.getListOfUnAssignedTasks(group_name, emergency_ind);
 		logger.info("All Tasks before shuffle running");
 		logger.info(tasks);
 		for (RaasTasksDTO obj : tasks) {
@@ -239,7 +323,7 @@ public class TaskAssignmentDAO {
 	public static void main(String... args) throws Exception {
 		TaskAssignmentDAO tskDAO = new TaskAssignmentDAO();
 		// tskDAO.shuffleTasksAccordingToPriorities();
-		tskDAO.runTaskAssignmentAlgorithm("ALL");
+		tskDAO.runTaskAssignmentAlgorithm("CPNI");
 
 	}
 }
